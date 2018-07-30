fdc00b70e310c07764e3a492cbd2e9207ca6a7bc
Dylan Marini <dylan.marini@g.austincc.edu>
2018-07-30 01:18:11 -0500

    progress 5

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,13 +7,14 @@
     var hero;
     var enemy;
     var cartman = {attack:10,defense:30};
-    var stan = {attack:20,defense:20};
+    var stan = {attack:100,defense:20};
     var kenny = {attack:25,defense:15};
     var kyle = {attack:30,defense:10};
     var heroAttack;
     var enemyAttack;
     var heroCurrentHealth = 100;
     var enemyCurrentHealth = 100;
+    var characterCount = 4;
 
     $('#hero_character').hide();
     $('#enemy_character').hide();
@@ -110,20 +111,14 @@
 
     $(".attack").on("click",function() {
 
-        console.log('3');
-
         heroAttack = hero.attack - enemy.defense;
         enemyAttack = enemy.attack - hero.defense;
-
-        console.log(heroAttack);
-        console.log(enemyAttack);
 
         heroCurrentHealth = heroCurrentHealth - enemyAttack;
         enemyCurrentHealth = enemyCurrentHealth - heroAttack;
 
-        console.log(heroCurrentHealth);
-        console.log(enemyCurrentHealth);
-
+        $("#hero_character").animate({ left: "+=400px" }, "fast" );
+        $("#hero_character").animate({ left: "-=400px" }, "fast" );
         
         $('#heroHealth').css('width', heroCurrentHealth + '%');
         $('#heroHealth').attr('aria-valuenow', heroCurrentHealth);
@@ -132,6 +127,77 @@
         $('#enemyHealth').css('width', enemyCurrentHealth + '%');
         $('#enemyHealth').attr('aria-valuenow', enemyCurrentHealth);
         $('.enemyPercentage').text(enemyCurrentHealth + '%')
+
+        if(enemyCurrentHealth <= 0){
+            characterCount = characterCount - 1;
+            $('#enemy_character').hide('slow');
+            
+            $(".attack").hide();
+
+            $(".character").on("click", function() {
+                character = ($(this).attr('id'));
+                if(character === "cartman"){
+                    $('#cartman').hide('slow');
+                    $("#enemy_character").attr('src',"https://vignette.wikia.nocookie.net/southpark/images/f/fd/The-coon.png/revision/latest?cb=20171107042514");
+                    $('#enemy_character').show('slow');
+                    enemy = cartman;
+                }
+                else if(character === "kyle"){
+                    $('#kyle').hide('slow');
+                    $("#enemy_character").attr('src',"https://vignette.wikia.nocookie.net/p__/images/e/e9/Spfbw-humankite.png/revision/latest?cb=20171028075530&path-prefix=protagonist");
+                    $('#enemy_character').show('slow');
+                    enemy = kyle;
+                }
+                else if(character === "stan"){
+                    $('#stan').hide('slow');
+                    $("#enemy_character").attr('src',"https://vignette.wikia.nocookie.net/southpark/images/b/bd/Toolshed.transparent.png/revision/latest?cb=20170527025409");
+                    $('#enemy_character').show('slow');
+                    enemy = stan;
+                }
+                else if(character === "kenny"){
+                    $('#kenny').hide('slow');
+                    $("#enemy_character").attr('src',"https://vignette.wikia.nocookie.net/southpark/images/1/1c/Mysterion_2.png/revision/latest?cb=20171107042555");
+                    $('#enemy_character').show('slow');
+                    enemy = kenny;
+                }
+                $(".attack").show();
+
+                $('#enemyHealth').css('width', 100 + '%');
+                $('#enemyHealth').attr('aria-valuenow', 100);
+                $('.enemyPercentage').text(100 + '%')
+
+                enemyCurrentHealth = 100;
+
+                $(".character").off("click");
+
+            })
+        
+        }
+
+        if(heroCurrentHealth <= 0){
+            $(".attack").hide();
+            $('#hero_character').hide('slow');
+            $('.header').text("CLICK TO RESTART");
+            
+            function blinker() {
+                $('.header').fadeOut(500);
+                $('.header').fadeIn(500);
+            }
+            
+            setInterval(blinker, 1000);
+
+        }
+
+        if(characterCount === 1){
+            $('.header').text("CLICK TO RESTART");
+
+            function blinker() {
+                $('.header').fadeOut(500);
+                $('.header').fadeIn(500);
+            }
+            
+            setInterval(blinker, 1000);
+        }
     })
   
 
